06aaebe88407b767c2a3142852fcbe2f85d5a1fa
Ubuntu <ubuntu@ip-172-31-8-35.eu-west-1.compute.internal>
2022-04-01 09:23:32 +0000

    dia 5

--- custom_facts/templates/prueba.ps1
+++ custom_facts/templates/prueba.ps1
@@ -1,1 +1,12 @@
-echo "ANTES_{{ VARIABLE_PLANTILLA }}_DESPUES"
+
+#echo "ANTES_{{ VARIABLE_PLANTILLA }}_DESPUES"
+
+$VERSION = '{{ VARIABLE_PLANTILLA }}'
+
+@{
+    dato1 = 'valor 1'
+    dato2 = @{
+        subdato1 = 'subvalor 1'
+        subdato2 = $VERSION
+    }
+}
